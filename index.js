a09a546fba590016710d3ae6ad15f32ac87737bd
evans <egatangi537@gmail.com>
2024-09-29 15:26:40 +0300

    changes

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solution in this file!
+let companyNames = ' Scuber '
+const companyName =' Scuber '
+
+let mostProfitableNeighbourhoods = 'Chelsea'
+const mostProfitableNeighbourhood = 'Chelsea'
+
+let companyCeos = 'Susan Smith'
+const companyCeo = 'Susan Smith'
